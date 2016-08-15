7db3409b1060728df9e7e45bd00bf15110f807b3
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-08-15 18:47:52 +0300

    Changed mongo url for the production.

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/columbia2'
+            'mongodb://admin:admin@ds033841.mlab.com:33841/testdb'
   }
 };
