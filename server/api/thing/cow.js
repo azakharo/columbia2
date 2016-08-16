434bf68c9dd6d0261c074b3a105c751865dee9d7
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-08-16 13:22:13 +0300

    Forgot to add the cow defines.

---
+++ server/api/thing/cow.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.SEX = ['M', 'F'];
+exports.PORODY = ['meet', 'milk'];
+exports.CHIP_LOCATIONS = ['back', 'stomach', 'head'];
+exports.HAIRS = ['black', 'white', 'black-white'];
+exports.SPEC_CHARS = [undefined, 'eye missing', 'tail missing'];
+exports.REPRODUCTION_CHOICES = [undefined, 'sterilized'];
+exports.GROUPS = ['inseminated', 'young', 'worker', 'quarantine'];
