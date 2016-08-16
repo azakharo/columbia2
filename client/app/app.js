896e66e4192febdc4f3696cbbd933bd562d59c5f
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-08-16 12:36:31 +0300

    client: disabled asking auth.

--- client/app/app.js
+++ client/app/app.js
@@ -15,7 +15,7 @@ angular.module('columbia2App', [
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
-    $httpProvider.interceptors.push('authInterceptor');
+    //$httpProvider.interceptors.push('authInterceptor');
   })
 
   .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
@@ -44,17 +44,17 @@ angular.module('columbia2App', [
     };
   })
 
-  .run(function ($rootScope, $location, Auth) {
-    // Redirect to login if route requires auth and you're not logged in
-    $rootScope.$on('$stateChangeStart', function (event, next) {
-      Auth.isLoggedInAsync(function(loggedIn) {
-        if (next.authenticate && !loggedIn) {
-          event.preventDefault();
-          $location.path('/login');
-        }
-      });
-    });
-  })
+  //.run(function ($rootScope, $location, Auth) {
+  //  // Redirect to login if route requires auth and you're not logged in
+  //  $rootScope.$on('$stateChangeStart', function (event, next) {
+  //    Auth.isLoggedInAsync(function(loggedIn) {
+  //      if (next.authenticate && !loggedIn) {
+  //        event.preventDefault();
+  //        $location.path('/login');
+  //      }
+  //    });
+  //  });
+  //})
 
   .run(function (i18nService) {
     // ui-grid
