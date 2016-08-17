9659311c16db01470d27934cf8bdd8b0bc3ae5ea
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-08-17 16:27:52 +0300

    dlg: fixed issue with datepicker's input.

--- client/app/main/animalInfoDlg.controller.js
+++ client/app/main/animalInfoDlg.controller.js
@@ -4,6 +4,8 @@ angular.module('columbia2App')
   .controller('animalInfoDlgCtrl',
   function ($scope, $uibModalInstance, animal) {
     $scope.animal = angular.copy(animal);
+    $scope.animal.birthday = new Date($scope.animal.birthday);
+    $scope.animal.chipDate = new Date($scope.animal.chipDate);
 
     $scope.onOk = function () {
       $uibModalInstance.close($scope.animal);
@@ -24,8 +26,8 @@ angular.module('columbia2App')
       showWeeks: false
     };
 
-    $scope.dateFormat = 'dd.MM.yyyy';
-    $scope.altInputFormats = ['d!.M!.yyyy'];
+    $scope.dateFormat = 'yyyy-MM-dd';
+    $scope.altInputFormats = ['yyyy-d!-M!'];
 
     // Date picker
     //===================================
